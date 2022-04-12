3a7188121b667bc7e211ec7c2cf8b00602ab5d53
Nikolos123 <Isponec2>
2022-04-12 21:01:18 +0300

    lesson_8

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,9 +1,12 @@
 import os
 import json
 
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import DetailView
+
 from mainapp.models import Product,ProductCategories
 
 MODULE_DIR = os.path.dirname(__file__)
@@ -19,15 +22,26 @@ def index(request):
     return  render(request,'mainapp/index.html',content)
 
 
-def products(request):
+def products(request,id_category=None,page=1):
 
-    products = read_file('fixtures/goods.json')
-    categories = read_file('fixtures/categories.json')
+    if id_category:
+        products_ = Product.objects.filter(category_id=id_category)
+    else:
+        products_ = Product.objects.all()
 
+    pagination = Paginator(products_,per_page=2)
+
+    try:
+        product_pagination = pagination.page(page)
+    except PageNotAnInteger:
+        product_pagination = pagination.page(1)
+    except EmptyPage:
+        product_pagination = pagination.page(pagination.num_pages)
     content = {
         'title' : 'Geekshop - Каталог',
         'categories': ProductCategories.objects.all(),
-        'products': Product.objects.all()
+        'products': product_pagination
+
     }
 
 
@@ -36,3 +50,6 @@ def products(request):
 
 #
 
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'mainapp/detail.html'
