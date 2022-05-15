a8cc09df88f914a864d30baca8579138daeb1e97
Nikolos123 <Isponec2>
2022-05-15 17:39:15 +0300

    update lesson_#

--- geekshop/basket/models.py
+++ geekshop/basket/models.py
@@ -5,9 +5,20 @@ from authapp.models import User
 from mainapp.models import Product
 
 
+# class BasketQuerySet(models.QuerySet):
+#
+#     def delete(self, *args, **kwargs):
+#         for item in self:
+#             item.product.quantity += item.quantity
+#             item.product.save()
+#         super(BasketQuerySet, self).delete(*args, **kwargs)
+
+
 class Basket(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    # objects = BasketQuerySet.as_manager()
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
     create_timestamp = models.DateTimeField(auto_now_add=True)
     update_timestamp = models.DateTimeField(auto_now=True)
@@ -16,7 +27,7 @@ class Basket(models.Model):
         return f'Корзина для  {self.user.username} | Продукт {self.product.name}'
 
     def sum(self):
-        return self.quantity*self.product.price
+        return self.quantity * self.product.price
 
     def get_basket(self):
         return Basket.objects.filter(user=self.user)
@@ -28,3 +39,21 @@ class Basket(models.Model):
     def total_quantity(self):
         baskets = self.get_basket()
         return sum(basket.quantity for basket in baskets)
+
+    # def save(self, *args, **kwargs):
+    #     if self.pk:
+    #         item = self.get_item(int(self.pk))
+    #         self.product.quantity -= self.quantity - item
+    #     else:
+    #         self.product.quantity -= self.quantity
+    #     self.product.save()
+    #     super(Basket, self).save(*args, **kwargs)
+    #
+    # def delete(self, *args, **kwargs):
+    #     self.product.quantity += self.quantity
+    #     self.save()
+    #     super(Basket, self).delete(*args, **kwargs)
+
+    @staticmethod
+    def get_item(pk):
+        return Basket.objects.get(pk=pk).quantity
--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -1,4 +1,6 @@
 from django.db import transaction
+from django.db.models.signals import pre_save, pre_delete
+from django.dispatch import receiver
 from django.forms import inlineformset_factory
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -39,7 +41,7 @@ class OrderCreate(CreateView, BaseClassContextMixin):
                     form.initial['product'] = basket_item[num].product
                     form.initial['quantity'] = basket_item[num].quantity
                     form.initial['price'] = basket_item[num].product.price
-                # basket_item.delete()
+                basket_item.delete()
             else:
                 formset = OrderFormSet()
 
@@ -111,3 +113,22 @@ def order_forming_complete(request, pk):
     order.status = Order.SEND_TO_PROCESSED
     order.save()
     return  HttpResponseRedirect(reverse('orders:list'))
+
+
+
+@receiver(pre_save,sender=OrderItem)
+@receiver(pre_save,sender=Basket)
+def product_quantity_update_save(sender, instance, **kwargs):
+        if instance.pk:
+            item = instance.get_item(int(instance.pk))
+            instance.product.quantity -= instance.quantity - item
+        else:
+            instance.product.quantity -= instance.quantity
+        instance.product.save()
+
+
+@receiver(pre_delete, sender=OrderItem)
+@receiver(pre_delete, sender=Basket)
+def product_quantity_update_delete(sender, instance, **kwargs):
+        instance.product.quantity += instance.quantity
+        instance.save()
